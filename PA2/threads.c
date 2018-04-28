a084d3263855cdd3d014995402e596f0787ba071
Malik Mann <malik.mann@st.ovgu.de>
2018-04-28 12:16:58 +0200

    Fixed formatting in PA2

--- PA2/threads.c
+++ PA2/threads.c
@@ -3,31 +3,31 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void* thredder(void* arg){
-    int* num = arg;
+void *thredder(void *arg) {
+    int *num = arg;
     int actualrealnumber = *num;
-    while(1){
+    while (1) {
         printf("Thread %d: Hello World!\n", actualrealnumber);
         sleep(5);
     }
 }
 
 
-int main(int argc, char* argv[]){
-if(argc==1){ 
-    printf("Wrong number of arguments.");
-    return 0;
-}
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("Wrong number of arguments.");
+        return 0;
+    }
 
-int num = atoi(argv[1]);
+    int num = atoi(argv[1]);
 
-pthread_t* thrpnt = malloc(num * sizeof(pthread_t));
+    pthread_t *thrpnt = malloc(num * sizeof(pthread_t));
 
-for(int i = 0; i<num; ++i){
-    pthread_create((thrpnt+i), NULL, thredder, &i);
-    sleep(0.001);
-}
+    for (int i = 0; i < num; ++i) {
+        pthread_create((thrpnt + i), NULL, thredder, &i);
+        sleep(0.001);
+    }
 
-while(1);
+    while (1);
 
 }
